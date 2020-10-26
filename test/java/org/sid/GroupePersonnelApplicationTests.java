bcd926d4ac90a86537b5429f1e429f24e8c2d2f5
maryamtaiaa <47307938+maryamtaiaa@users.noreply.github.com>
2020-10-26 13:30:21 +0100

    Add files via upload

---
+++ test/java/org/sid/GroupePersonnelApplicationTests.java
@@ -0,0 +1,16 @@
+package org.sid;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+class GroupePersonnelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
